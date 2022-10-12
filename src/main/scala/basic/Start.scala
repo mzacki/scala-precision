605d1495452dca6c38dccfe283fcbc938aea65c1
mzacki <38253922+mzacki@users.noreply.github.com>
2022-10-12 20:33:51 +0200

    start

---
+++ src/main/scala/basic/Start.scala
@@ -0,0 +1,13 @@
+package edu.ant
+package basic
+
+object Start {
+
+  @main
+  def main(): Unit = println("Press fire to continue")
+
+  // or
+
+  def main(args: Array[String]): Unit = println("Move joystick to continue")
+
+}
