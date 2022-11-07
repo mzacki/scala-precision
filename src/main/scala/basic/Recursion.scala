6c0791f8c565dab879057af6d884d1144a017c7b
mzacki <38253922+mzacki@users.noreply.github.com>
2022-11-07 18:44:33 +0100

    recursion

--- src/main/scala/basic/Recursion.scala
+++ src/main/scala/basic/Recursion.scala
@@ -9,7 +9,7 @@ object Recursion extends App {
   // @tailrec - complains because recursive call is not the last expression
   def factorial(n: Int): Int = if (n <= 1) 1 else factorial(n - 1) * n
 
-  println("Factorial: " + correctFactorial(10 ))
+  println("Factorial: " + correctFactorial(10))
 
   // wrapper function preserves same stack for execution - tail recursion
   // instead of loops, use tail recursion
@@ -22,5 +22,39 @@ object Recursion extends App {
     helper(n, 1)
   }
 
+  // concatenate String n times
+  @tailrec
+  def concatenate(word: String, n: Int, accumulator: String): String = {
+    if (n <= 0) accumulator
+    else concatenate(word, n - 1, word + accumulator)
+  }
+
+  println(concatenate("Oz", 2, ""))
+
+  // prime checker recursive
+
+  def isPrime(n: Int): Boolean = {
+
+    @tailrec
+    def isPrimeRecursive(m: Int, isStillPrime: Boolean): Boolean =
+
+      if (!isStillPrime) false
+      else if (m <= 1) true
+      else isPrimeRecursive(m - 1, n % m != 0 && isStillPrime)
+
+    isPrimeRecursive(n / 2, true)
+
+  }
+
+  // fibonacci recursive
+  def fibo(n: Int): Int = {
+    @tailrec
+    def fiboRecursive(i: Int, last: Int, nextToLast: Int): Int =
+      if (i >= n) last
+      else fiboRecursive(i + 1, last + nextToLast, last)
+
+    if (n <= 2) 1
+    else fiboRecursive(2, 1, 1)
+  }
 
 }
