86573e5202db01123f39e9781999a8c1b89349f5
mzacki <38253922+mzacki@users.noreply.github.com>
2022-12-22 16:33:18 +0100

    oop

--- src/main/scala/intro/oop/ObjectsExplained.scala
+++ src/main/scala/intro/oop/ObjectsExplained.scala
@@ -16,10 +16,38 @@ object ObjectsExplained extends App{
 
   private val ant = Ant
   private val ant2 = Ant
-  println("Identity: " + (ant == ant2)) // identity
-  println("Identity (equals() method): " + ant.equals(ant2)) // identity
-  println("Reference equality (eq() method): " + ant.eq(ant2)) // reference equality
+  println("Identity: " + (ant == ant2)) // identity: true
+  println("Identity (equals() method): " + ant.equals(ant2)) // identity: true
+  println("Reference equality (eq() method): " + ant.eq(ant2)) // reference equality: true - same instance (singleton)
 
+  private class Ant {
+    // instance-level functionality
+  }
 
+  // Both Ants are COMPANIONS
+
+  private val ant3 = new Ant
+  private val ant4 = new Ant
+  println("Identity: " + (ant3 == ant4)) // identity: false
+  println("Identity (equals() method): " + ant3.equals(ant4)) // identity: false
+  println("Reference equality (eq() method): " + ant3.eq(ant4)) // reference equality - not the same instance! false
+  // note: == and equals() default behaviour is eq (reference comparison): https://stackoverflow.com/questions/9853645/scala-does-default-to-equals
+
+  object Bee {
+    // factory method
+    def from(breed1: Bee, breed2: Bee): Bee = new Bee("Carnica")
+    // in practice, the method's name could be apply(...)
+  }
+
+  class Bee(val breed: String) {
+
+  }
+
+  val bee1 = new Bee("Sklenar")
+  val bee2 = new Bee("Carnica")
+  val newBee = Bee.from(bee1, bee2)
+
+  // possible applications Scala object with
+  // def main(args: Array[String]): Unit
 
 }
