bece012558dfb3f19554da16e9256810ae5ab4e7
mzacki <38253922+mzacki@users.noreply.github.com>
2022-12-29 20:41:31 +0100

    abstracts and traits

---
+++ src/main/scala/intro/oop/Abstract.scala
@@ -0,0 +1,34 @@
+package edu.ant
+package intro.oop
+
+object Abstract extends App {
+
+  abstract class Ant {
+    def doSomething(): Unit
+  }
+
+  class FireAnt extends Ant {
+    override def doSomething(): Unit = println("Burn them!")
+  }
+
+  trait Insect
+  trait Hymenoptera
+
+  class Bee extends Insect with Hymenoptera {}
+
+  /**
+   * Traits vs Abstract class:
+   * traits don't have contructor params
+   * multiple traits can be implemented
+   * */
+
+  /**
+   * Scala types hierarchy:
+   * scala.Any
+   * scala.AnyRef (== java.lang.Object)
+   * scala.Null
+   * scala.AnyVal
+   * scala.Nothing
+   * */
+
+}
