e91f07b0314412dedf84ac76b72da819242c3402
mzacki <38253922+mzacki@users.noreply.github.com>
2023-01-09 18:03:15 +0100

    bounding types

--- src/main/scala/structures/list/generic/GenericListImpl.scala
+++ src/main/scala/structures/list/generic/GenericListImpl.scala
@@ -30,6 +30,13 @@ object GenericListImpl extends App{
   // or simply:
   // val insectList = new CovariantList[Ant]
 
+  /**
+   * What if a Bee would be added to Ant list?
+   * (val insectList: CovariantList[Insect] = new CovariantList[Ant] is in fact ANT list, not INSECT list)
+   * def add[I >: A](item: I): CovariantList[B] == ???
+   * ANT list should be turned into INSECT LIST
+   */
+
   // case #2 (invariance)
   class InvariantList[A]
   val invariantInsectList: InvariantList[Insect] = new InvariantList[Insect]
@@ -39,5 +46,13 @@ object GenericListImpl extends App{
   // case #3 (contravariance)
   class ContravariantList[-A]
   val contravariantList: ContravariantList[Bee] = new ContravariantList[Insect]
+
+  // bounded types - class Anthill constructor accepts all generics extending Ant class (all subclasses of Ant)
+  class Anthill[A <: Ant] (ant: A)
+  // example
+  val anthill = new Anthill(new Ant)
+  // accepts all generics being a superclass of Ant
+  class Insectarium[I >: Ant] (instect: I)
+  val insectarium = new Insectarium(new Insect())
 
 }
